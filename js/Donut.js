ebac31b7a884a147cbb35c1e0c953b7f901614ce
Pedro <pwbarbosa@gmail.com>
2022-07-07 15:33:10 -0400

    Iteration 1 feature 2 and 3 and 4 commit oops. they were kind of interwoven conceptually

--- js/Donut.js
+++ js/Donut.js
@@ -1,14 +1,33 @@
 class Donut{
     constructor() {
-        this._count = 0;
+        this._count = 90;
+        this._autoClickCount = 0;
+        this._autoClickCost = 100;
     }
 
     get count() {
         return this._count;
     }
+    get aCCount() {
+        return this._aCCount;
+    }
 
     click() {
         this._count++;
+    }
+
+    purchaseAutoClicker() {
+        if (this._count >= this._autoClickCost) {
+            this._count -= this._autoClickCost;
+            this._autoClickCost *= 1.1;
+            this._autoClickCount++;
+        }
+        else {
+            alert("Oops! You need " + (this._autoClickCost - this._count) + " more donuts to purchase this!");
+        }
+        
+
+
     }
 
 }
--- js/app.js
+++ js/app.js
@@ -2,13 +2,23 @@ import Donut from "./Donut.js"
 // const container = (document.querySelector('.container').innerText =
 //   'mmmmmmmmm doughnuts.... ahhhhhhh!');
 const countEl = document.querySelector("#count");
+const autoClickCountEl = document.querySelector("#clickersPurchased")
 const donut = new Donut();
 
 countEl.innerHTML = donut.count;
+autoClickCountEl.innerHTML = donut._autoClickCount;
+
 
 const makeBtn = document.querySelector("#make");
 makeBtn.addEventListener("click", () => {
     donut._count++;
+    countEl.innerHTML = donut._count;
+})
+
+const clickerPurchaseBtn = document.querySelector("#autoClickerPuchase");
+clickerPurchaseBtn.addEventListener("click", () => {
+    donut.purchaseAutoClicker();
+    autoClickCountEl.innerHTML = donut._autoClickCount;
     countEl.innerHTML = donut._count;
 })
 
